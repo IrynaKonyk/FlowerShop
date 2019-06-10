132728337daacf18f072d68c9088b31ac60ffaf7
Iryna Konyk <irynakonik1996@gmail.com>
2019-06-10 19:27:24 +0300

    add abstract method price()

--- src/products/Product.java
+++ src/products/Product.java
@@ -1,27 +1,27 @@
 package products;
 //abstract class, which is common for all goods in shop
-//it has only name of goods and its price.
+//it has only description of goods and its price.
 public abstract class Product {
-    private String name;
+    private String description;
     private double price;
+    abstract double price();
 
-    public Product(String name, double price) {
-        this.name = name;
+    public Product(String description, double price) {
+        this.description = description;
         this.price = price;
     }
 
-    public String  getName() {
-        return name;
+    public String getDescription() {
+        return description;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public double getPrice() {
         return price;
     }
-
+    
     public void setPrice(double price) {
         this.price = price;
     }
@@ -29,7 +29,7 @@ public abstract class Product {
     @Override
     public String toString() {
         return "Product{" +
-                "name=" + name +
+                "description=" + description +
                 ", price=" + price +
                 '}';
     }
