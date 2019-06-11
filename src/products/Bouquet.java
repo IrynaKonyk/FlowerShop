5a7aa87c44559b7d95f11e4bab2de1ab1daa4702
Iryna Konyk <irynakonik1996@gmail.com>
2019-06-12 01:47:13 +0300

    add addFlowers()

--- src/products/Bouquet.java
+++ src/products/Bouquet.java
@@ -4,11 +4,32 @@ import java.util.ArrayList;
 
 public class Bouquet extends Product {
     private ArrayList<Flower> flowers;
-    private ArrayList<ProductDecorator> decoration;
 
-    public Bouquet(String name, double price, ArrayList<Flower> flowers, ArrayList<ProductDecorator> decoration) {
-        super(name, price);
+    public Bouquet(String description, ArrayList<Flower> flowers) {
+        super(description);
         this.flowers = flowers;
-        this.decoration = decoration;
     }
+
+    public ArrayList<Flower> getFlowers() {
+        return flowers;
+    }
+
+    public void setFlowers(ArrayList<Flower> flowers) {
+        this.flowers = flowers;
+    }
+
+    public void addFlowers(ArrayList<Flower> flowers2) {
+        flowers.addAll(flowers2);
+    }
+
+    @Override
+    public double price() {
+        double totalPrice = 0;
+        for (Flower flower : flowers) {
+            totalPrice += flower.price();
+        }
+        return totalPrice;
+
+    }
+
 }
