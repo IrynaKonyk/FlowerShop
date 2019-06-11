31da657875e323e0c801c092e5fe165e8a6cb29a
Iryna Konyk <irynakonik1996@gmail.com>
2019-06-11 19:03:37 +0300

    completely final version

--- src/products/Product.java
+++ src/products/Product.java
@@ -1,41 +1,27 @@
 package products;
 //abstract class, which is common for all goods in shop
-//it has only description of goods and its price.
+//it has only description of goods and its method to calculate price.
 public abstract class Product {
     private String description;
-    private double price;
-    abstract double price();
 
+    public abstract double price();
 
-    public Product() {
-
-    }
-
-    public Product(String description, double price) {
+    public Product(String description) {
         this.description = description;
-        this.price = price;
     }
 
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
     }
 
     @Override
     public String toString() {
         return "Product{" +
                 "description=" + description +
-                ", price=" + price +
                 '}';
     }
 }
